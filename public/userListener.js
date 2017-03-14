9cae091e8928a0d0aedb1ce5f7539a34c7ac5f95
Edgar Dewsbery <edewsbery@gmail.com>
2017-03-14 14:56:51 +0200

    create keyup eventListener (currently console.log)

---
+++ public/userListener.js
@@ -0,0 +1,10 @@
+var inputField = document.getElementById('autoCompleteInput');
+
+
+inputField.addEventListener("keyup", function(event){
+  console.log(event.target.value);
+})
+
+
+
+// autoCompleteInput
