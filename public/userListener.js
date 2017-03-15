e55d314841ce70283c4e3874772148c0d5f86029
Edgar Dewsbery <edewsbery@gmail.com>
2017-03-15 16:32:26 +0200

    initialise render function

--- public/userListener.js
+++ public/userListener.js
@@ -1,9 +1,13 @@
 var inputField = document.getElementById('autoCompleteInput');
 
+function requestCallback(response) {
+  render(response);
+}
+
 
 inputField.addEventListener("keyup", function(event){
-  if(event.keyCode >= 65 && event.keyCode <= 90) {
-    console.log(event.target.value);
+  if(event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode === 8) {
+    makeRequest(event.target.value, requestCallback);
   }
 })
 
